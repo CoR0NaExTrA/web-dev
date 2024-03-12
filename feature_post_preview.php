28abbeaa62279b3bfdcea956df97bcf23cea6dde
CoR0NaExTrA <159535808+CoR0NaExTrA@users.noreply.github.com>
2024-03-12 19:57:23 +0300

    -main

--- feature_post_preview.php
+++ feature_post_preview.php
@@ -1,6 +1,6 @@
 <article class="feature-posts__item_img" style="background-image: url(<?= $feature_post['background_post'] ?>)">
         <div class="feature-posts__tag__<?= $feature_post['tag_type'] ?>">
-            <span class="feature-posts__tag_txt">A<?= $feature_post['tag_text'] ?></span>
+            <span class="feature-posts__tag_txt"><?= $feature_post['tag_text'] ?></span>
         </div>
         <h2 class="article-info__main-txt"><?= $feature_post['title'] ?></h2>
         <span class="article-info__bot-txt"><?= $feature_post['subtitle'] ?></span>
