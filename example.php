174e6df83bd686a9cd4b0d8467f245ff9dafa1a7
CoR0NaExTrA <159535808+CoR0NaExTrA@users.noreply.github.com>
2024-03-13 19:53:43 +0300

    -m

--- example.php
+++ example.php
@@ -31,3 +31,13 @@ echo "Какой-то timestamp на след неделе {$timestampNextWeek} 
 echo mktime(9, 24, 57, 5, 23, 1995) . '<br>';
 
 echo '<br>---------------<br>';
+
+$method = $_SERVER['REQUEST_METHOD'];
+$url = $_SERVER['REQUEST_URI'];
+$ipAddress = $_SERVER['REMOTE_ADDR'];
+
+echo 'Method: ' . $method . '</br>';
+echo 'URL: ' . $url . '</br>';
+echo 'IP Address: ' . $ipAddress . '</br>';
+
+echo '<br>---------------<br>';
