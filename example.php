db50cae07f6bed260483cfe1f8db9a863b4dd845
CoR0NaExTrA <159535808+CoR0NaExTrA@users.noreply.github.com>
2024-03-13 19:48:33 +0300

    -m

--- example.php
+++ example.php
@@ -13,8 +13,17 @@ echo "Timestamp запроса {$requestTime} <br>";
 $now = date("Y-m-d H:i:s");
 echo "Текущая дата и время: {$now} <br>";
 
-$requestDateTime = date("Y-m-d H:i:s", 1700000000);
+$requestDateTime = date("Y-m-d H:i:s", 2000000000);
 echo "Дата и время запроса: {$requestDateTime} <br>";
 
 $lastDay = date("H*i*s Y/m/d", 1717189199);
 echo "Последний день учебы: {$lastDay} <br>";
+
+$timestampNow = strtotime('now');
+echo "Текущий timestamp: {$timestampNow} <br>";
+
+$timestampDatetime = strtotime($now);
+echo "Текущий timestamp: {$timestampDatetime} <br>";
+    
+$timestampNextWeek = strtotime('+1 week 2 days 4 hours 2 seconds');
+echo "Какой-то timestamp на след неделе {$timestampNextWeek} <
