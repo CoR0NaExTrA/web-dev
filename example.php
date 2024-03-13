79b29e0b05b973c4cb44474210aecc95f7688818
CoR0NaExTrA <159535808+CoR0NaExTrA@users.noreply.github.com>
2024-03-13 19:38:01 +0300

    -main

--- example.php
+++ example.php
@@ -5,3 +5,7 @@ foreach ($_SERVER as $key => $header) {
   echo "{$key} = {$header} </br>";
 }
 echo '<br>---------------<br>';
+
+// 2. вывести время запроса
+$requestTime = $_SERVER['REQUEST_TIME'];
+echo "Timestamp запроса {$requestTime} <br>";
