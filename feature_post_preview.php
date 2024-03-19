908ba7e4a7cfbc7b8f79049ea69e96a30ed68b6e
CoR0NaExTrA <159535808+CoR0NaExTrA@users.noreply.github.com>
2024-03-19 22:52:21 +0300

    -m

--- feature_post_preview.php
+++ feature_post_preview.php
@@ -2,7 +2,7 @@
         <div class="feature-posts__tag__<?= $feature_post['tag_type'] ?>">
             <span class="feature-posts__tag_txt"><?= $feature_post['tag_text'] ?></span>
         </div>
-        <h2 class="article-info__main-txt"><?= foreach ($_GET as $key => $value) {echo "{$key} = {$value}"; }?><?= $feature_post['title'] ?></h2>
+        <h2 class="article-info__main-txt"><?= $feature_post['title'] ?></h2>
         <span class="article-info__bot-txt"><?= $feature_post['subtitle'] ?></span>
         <div class="article-info">
             <div class="author-info">
