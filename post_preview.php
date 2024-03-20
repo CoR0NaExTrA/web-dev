f3bf700e98fa0d639b5a88a6e047d62bc7e5dd09
CoR0NaExTrA <159535808+CoR0NaExTrA@users.noreply.github.com>
2024-03-20 11:45:16 +0300

    -m

--- post_preview.php
+++ post_preview.php
@@ -2,7 +2,7 @@
         <img class="most-recent__item_img" src="<?= $post['background_post'] ?>" alt="img">
         <div class="most-recent__item_content-area">
             <h3 class="most-recent__main-txt"><?= $post['title']?></h3>
-            <span class="most-recent__bot-txt"><?= $post['title'] ?></span>
+            <span class="most-recent__bot-txt"><?= $post['subtitle'] ?></span>
             <div class="most-recent__item_author-info">
                 <div class="most-recent__author-info">
                     <img class="author-info__img" src="<?= $post['img_author']?>" alt="img">
