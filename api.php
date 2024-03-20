ee7d4d9aa7522e95b37360b5a6bbadb82e996652
CoR0NaExTrA <159535808+CoR0NaExTrA@users.noreply.github.com>
2024-03-20 13:02:18 +0300

    -main

--- api.php
+++ api.php
@@ -9,6 +9,7 @@
     $dataAsJson = file_get_contents("php://input");
     $dataAsArray = json_decode($dataAsJson, true);
     saveImage($dataAsArray['image']);
+    saveFile('data.json', $dataAsJson);
 
     function saveImage(string $imageBase64) {
     $imageBase64Array = explode(';base64,', $imageBase64);
