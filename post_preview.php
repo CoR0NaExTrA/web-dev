dd83e054a29aa784a3fc230d7225b70c76bfa174
CoR0NaExTrA <159535808+CoR0NaExTrA@users.noreply.github.com>
2024-04-09 20:53:27 +0300

    -m

--- post_preview.php
+++ post_preview.php
@@ -4,9 +4,9 @@
 <h6 class="lessboldheadline">
     <?= $post['subtitle']?>
 </h6>
-<img src=<?= $post['image']?> width="100%" height="534" alt="image">
+<img src=<?= $post['image_url']?> width="100%" height="534" alt="image">
 <div class="main-text">
     <p>
-        <?= $post['text']?>
+        <?= $post['content']?>
     </p>
 </div>
