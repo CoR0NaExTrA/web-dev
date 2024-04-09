0811d5440c8911e98282f6b1d68cbf8e1aa92cea
CoR0NaExTrA <159535808+CoR0NaExTrA@users.noreply.github.com>
2024-04-09 20:37:51 +0300

    -m

--- feature_post_preview.php
+++ feature_post_preview.php
@@ -1,4 +1,4 @@
-<article class="feature-posts__item_img" style="background-image: url(<?= $feature_post['background_post'] ?>)">
+<article class="feature-posts__item_img" style="background-image: url(<?= $feature_post['image_url'] ?>)">
     <div class="feature-posts__tag__<?= $feature_post['tag_type'] ?>">
         <span class="feature-posts__tag_txt"><?= $feature_post['tag_text'] ?></span>
     </div>
@@ -6,10 +6,10 @@
     <span class="article-info__bot-txt"><?= $feature_post['subtitle'] ?></span>
     <div class="article-info">
         <div class="author-info">
-            <img class="author-info__img" src="<?= $feature_post['img_author'] ?>" alt="img">
+            <img class="author-info__img" src="<?= $feature_post['author_url'] ?>" alt="img">
             <span class="author-info__txt"><?= $feature_post['author'] ?></span>
         </div>
-        <span class="article-info__date"><?= date('m/d/Y', $feature_post['post_date'])?></span>
+        <span class="article-info__date"><?= date('m/d/Y', $feature_post['publish_date'])?></span>
     </div>
-    <a class="post_link" href='/post.php?id=<?= $feature_post['id'] ?>'><span></span></a>
+    <a class="post_link" href='/post.php?id=<?= $feature_post['post_id'] ?>'><span></span></a>
 </article>
--- most_recent_post_preview.php
+++ most_recent_post_preview.php
@@ -1,5 +1,5 @@
 <article class="most-recent__item">
-    <img class="most-recent__item_img" src="<?= $post['background_post'] ?>" alt="img">
+    <img class="most-recent__item_img" src="<?= $post['image_url'] ?>" alt="img">
     <div class="most-recent__item_content-area">
         <h3 class="most-recent__main-txt">
             <?= $post['title'] ?>
@@ -9,15 +9,15 @@
         </span>
         <div class="most-recent__item_author-info">
             <div class="most-recent__author-info">
-                <img class="author-info__img" src="<?= $post['img_author'] ?>" alt="img">
+                <img class="author-info__img" src="<?= $post['author_url'] ?>" alt="img">
                 <span class="most-recent__author-info_txt">
                     <?= $post['author'] ?>
                 </span>
                 <span class="most-recent__author-info_date">
-                    <?= date("m/d/Y", $post['post_date']) ?>
+                    <?= date("m/d/Y", $post['publish_date']) ?>
                 </span>
             </div>
         </div>
     </div>
-    <a class="post_link" href='/post.php?id=<?= $post['id'] ?>'><span></span></a>
+    <a class="post_link" href='/post.php?id=<?= $post['post_id'] ?>'><span></span></a>
 </article>
