bd5b5d3cb63557e0d862390f555ab9e73ebb3662
Zzenam <yroslavtrofim@gmail.com>
2024-04-29 16:52:31 +0300

    -main

---
+++ scripts/admin.js
@@ -0,0 +1,15 @@
+const titleInput = document.getElementById('title');
+const subtitleInput = document.getElementById('subtitle');
+const authorNameInput = document.getElementById('author_name');
+const publishDateInput = document.getElementById('publish_date');
+
+const postImageInput = document.getElementById('post-img');
+const previewImageInput = document.getElementById(preview-img);
+const authorAvatarInput = document.getElementById('author-avatar');
+
+
+
+
+
+
+
